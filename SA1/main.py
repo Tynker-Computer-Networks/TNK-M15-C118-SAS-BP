233eef1308374e0642e323ba503cc233f6ec3c31
vishalgddam873 <vishal18@navgurukul.org>
2023-11-08 12:06:38 +0530

    Code Updated

--- SA1/main.py
+++ SA1/main.py
@@ -46,7 +46,6 @@ class EmailSenderApp(Tk):
             smtp_server.starttls()
             smtp_server.login(sender_email, sender_password)
 
-            # Create and send the email
             message = MIMEMultipart()
             message["From"] = sender_email
             message["To"] = recipient_email
